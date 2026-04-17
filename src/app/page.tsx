3b0057853f7335fff4cb5b6cd6367aa75b85c619
Teeravee Sirinapasawasdee <teeravee@thaicloud.com>
2026-04-17 14:07:23 +0700

    Implement new template (minimal-creative)

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { StandardPage, buildMetadata } from "@/components/site-shell";
-
-export const metadata = buildMetadata("home");
-
-export default function HomePage() {
-  return <StandardPage pageKey="home" />;
-}
